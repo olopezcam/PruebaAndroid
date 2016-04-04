729b4e8e9f9e4bf37e57fbb5cb695e8f931e682f
olopezcam <olopezcam@gmail.com>
2016-04-04 19:17:24 +0200

    Dos botones

--- app/src/main/java/com/example/oscar/prueba/MainActivity.java
+++ app/src/main/java/com/example/oscar/prueba/MainActivity.java
@@ -2,12 +2,16 @@ package com.example.oscar.prueba;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
+    Button boton;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        boton= (Button) findViewById(R.id.button);
     }
+
 }
